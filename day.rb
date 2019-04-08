de838dbaa6f0bc23ac559c828bfbb99786cb4e06
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-04-08 10:25:34 +0530

    added new function for greeting

--- day.rb
+++ day.rb
@@ -3,3 +3,7 @@ require "date"
 def day_of_week(time)
   return Date::DAYNAMES[time.wday]
 end
+
+def greeting(time)
+  return "Hi There, Wishing you a great #{day_of_week(time)}"
+end
--- hello_app.rb
+++ hello_app.rb
@@ -2,5 +2,5 @@ require 'sinatra'
 require './day'
 
 get '/' do
-  "Hello world!, Happy #{day_of_week(Time.now)} Now from a file"
+  greeting(Time.now)
 end
