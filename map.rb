c0f0b5f41b477724195a01fdc60480f82e08c42e
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-04-08 14:42:24 +0530

    functional programing examples added

---
+++ map.rb
@@ -0,0 +1,28 @@
+
+states = ["Kansas", "Nebraska", "North Dakota", "South Dakota"]
+
+def urlify(string)
+  string.downcase.split.join('-')
+end
+
+def addressify(string)
+  "https://example.com/#{string}"
+end
+
+#imperative
+def imperative_urls(states)
+  urls = []
+  states.each do |state|
+    urls << addressify(urlify(state))
+  end
+  urls
+end
+
+p imperative_urls(states)
+
+#functional
+def functional_url(states)
+  states.map {|state| urlify(state)} .map {|state| addressify(state)}
+end
+
+p functional_url(states)
